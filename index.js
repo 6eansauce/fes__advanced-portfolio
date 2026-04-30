992261bba8d61c3d898868b6c747f531c1f2164a
6eansacue <oscaritojovel@gmail.com>
2026-04-30 08:37:54 -0400

    Modal js complete

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-// template_i615z1b
-
 const contact = (event) => {
   event.preventDefault();
-  emailjs.sendForm("service_gslu7e8", "template_i615z1b", event.target);
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList.toggle("u-visibility-visible");
+
+  emailjs
+    .sendForm(
+      "service_gslu7e8",
+      "template_dns02k4",
+      event.target,
+      "OIQPHws5LfIX9MeUL",
+    )
+    .then(() => {
+      loading.classList.remove("u-visibility-visible");
+      success.classList.toggle("u-visibility-visible");
+    })
+    .catch(() => {
+      loading.classList.remove("u-visibility-visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on oscaritojovel@gmail.com",
+      );
+    });
+};
+
+const toggleModal = () => {
+  const modal = document.querySelector(".modal");
+
+  const header = document.querySelector(".header");
+  const hero = document.querySelector(".hero");
+  const scroll = document.querySelector(".scroll");
+
+  modal.classList.toggle("modal--open");
+  header.classList.toggle("u-visibility-hidden");
+  hero.classList.toggle("u-visibility-hidden");
+  scroll.classList.toggle("u-visibility-hidden");
 };
